b11d3ace4a9c5f70e3b97257549868d3058a69b6
David Buss <kotarcreative@gmail.com>
2019-04-01 19:15:09 -0600

    Add product management page.

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import '../pages/products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              title: Text('Choose'),
+              automaticallyImplyLeading: false,
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ProductsAdminPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
